75ab736c22a848ad053111e4615bea29ede78354
Sophia Fu <sophiafu0523@gmail.com>
2026-03-02 16:29:19 -0500

    Update lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Sophia Fu" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hi everyone, my name is Sophia Fu and I am a junior from Massachusetts. My favorite classes at school are currently AP Chemistry and Mechanical Engineering. After school, I enjoy being part of Newton's FRC team, the LigerBots. This season is going pretty well so far. We won week zero and we have our first real competition March 14-15th! Outside of academics I love to watercolor animals and dance ballet. My favorite ballet is Coppélia and I would encourage everyone to watch it. Looking forward to getting to know you all!")
 
 if __name__ == "__main__":
     main()
